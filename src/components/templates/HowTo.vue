3fa2e4daf811a81b393801dfef37fc58468ada54
10kota <surfacenk@gmail.com>
2023-07-05 22:33:13 +0900

    Description column added

--- src/components/templates/HowTo.vue
+++ src/components/templates/HowTo.vue
@@ -1,6 +1,27 @@
-<template>
+<!DOCTYPE HTML>
+<template id="template">
   <div>
-    使い方
+    <table border="1">
+    <tr>
+      <th colspan="2">使用方法</th>
+    </tr>
+    <tr>
+      <td>名前</td>
+      <td>送信者の名前をご記入下さい。</td>
+    </tr>
+    <tr>
+      <td>送信</td>
+      <td>送信するデータを選択して下さい。</td>
+    </tr>
+    <tr>
+      <td>受信</td>
+      <td>受信するデータを選択して下さい。</td>
+    </tr>
+    <tr>
+      <td>設定</td>
+      <td>ご自分の名前とポート番号を識別できます。</td>
+    </tr>
+    </table>
   </div>
 </template>
 
@@ -13,4 +34,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>

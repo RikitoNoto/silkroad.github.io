e3a52e680861bb12d8ddc15face75c8e0b67110e
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2023-09-22 22:33:36 +0900

    release 1.2.0

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/global.css'
+// import './assets/global.css' //FIXME: debug時はこれよりbootstrapが優先されるが、buildするとこれが最優先になる。
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import { library } from '@fortawesome/fontawesome-svg-core'
